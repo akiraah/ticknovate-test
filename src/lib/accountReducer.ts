436f438392f820b3c97cac6b995df8e6aa61f2f8
akiraah <rah24.a@gmail.com>
2022-06-10 21:26:49 +0100

    init commit

---
+++ src/lib/accountReducer.ts
@@ -0,0 +1,40 @@
+import { BankAccountEvent, IBankAccount } from '../types'
+
+enum AccountTypes {
+  AccountOpened = 'AccountOpened',
+  MoneyDebited = 'MoneyDebited',
+  MoneyCredited = 'MoneyCredited',
+}
+
+export const accountReducer = (events: BankAccountEvent[]): IBankAccount =>
+  events.reduce((acc, val) => {
+    const { type } = val
+    acc['status'] = 'open'
+    acc['accountId'] = val.accountId
+    if (type === AccountTypes.AccountOpened) {
+      acc['ownerName'] = val.ownerName
+      acc['openedAt'] = Date.parse(val.time)
+      acc['transactions'] = []
+      acc['balance'] = 0
+    }
+    if (type === AccountTypes.MoneyDebited) {
+      const transaction = {
+        type: 'debit' as const,
+        value: val.value,
+        timestamp: Date.parse(val.time),
+      }
+      acc['balance'] -= val.value
+      acc['transactions'].push(transaction)
+    }
+    if (type === AccountTypes.MoneyCredited) {
+      const transaction = {
+        type: 'credit' as const,
+        value: val.value,
+        timestamp: Date.parse(val.time),
+      }
+      acc['balance'] += val.value
+      acc['transactions'].push(transaction)
+    }
+    acc['isOverdrawn'] = acc['balance'] < 0
+    return acc
+  }, {} as IBankAccount)
---
+++ src/lib/events.ts
@@ -0,0 +1,55 @@
+import fs from 'fs/promises'
+import path from 'path'
+import { AppError } from './errorHandling'
+import type { BankAccountEvent } from '../types'
+
+/**
+ * Load events for the given `accountId`.
+ *
+ * TODO: Implement this function. This is part of the test.
+ *
+ * The implementation should return a promise that resolves to an array
+ * of objects, sourced from the relevant directory inside of the "events"
+ * directory at the root of this project.
+ *
+ * @see saveEvents
+ */
+export async function loadEvents(
+  accountId: string
+): Promise<BankAccountEvent[]> {
+  const filePath = path.join(process.cwd(), `/events/${accountId}`)
+  try {
+    const files: string[] = await fs.readdir(filePath)
+    const future: Promise<BankAccountEvent>[] = files.map(async (file) => {
+      const fileBuffer: Buffer = await fs.readFile(`${filePath}/${file}`)
+      const event: BankAccountEvent = JSON.parse(
+        Buffer.from(fileBuffer).toString()
+      )
+      return event
+    })
+    return Promise.all(future)
+  } catch (error) {
+    throw new AppError(400, `Unable to read events for ${accountId}`)
+  }
+}
+
+/**
+ * Saves new events.
+ */
+export async function saveEvents(events: BankAccountEvent[]) {
+  await Promise.all(
+    events.map(async (event) => {
+      const filepath = path.join(
+        __dirname,
+        '../../events',
+        event.accountId,
+        `${event.position}.json`
+      )
+      console.log('Writing new event to', filepath)
+      await fs.writeFile(filepath, JSON.stringify(event, null, 2), {
+        // Fail if the file already exists
+        flag: 'wx',
+      })
+    })
+  )
+}
